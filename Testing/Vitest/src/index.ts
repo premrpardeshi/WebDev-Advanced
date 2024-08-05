d458a5331ebeba6a8fe2428430b23db766f6f6f6
Prem Pardeshi <prempardeshi283@gmail.com>
2024-08-05 14:40:13 +0530

    Vitest Spy

--- Testing/Vitest/src/index.ts
+++ Testing/Vitest/src/index.ts
@@ -6,49 +6,50 @@ export const app = express();
 app.use(express.json());
 
 const sumInput = z.object({
-    a: z.number(),
-    b: z.number()
-})
+  a: z.number(),
+  b: z.number(),
+});
 
 app.post("/sum", async (req, res) => {
-    const parsedResponse = sumInput.safeParse(req.body)
-    
-    if (!parsedResponse.success) {
-        return res.status(411).json({
-            message: "Incorrect inputs"
-        })
-    }
+  const parsedResponse = sumInput.safeParse(req.body);
 
-    const answer = parsedResponse.data.a + parsedResponse.data.b;
+  if (!parsedResponse.success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
 
-    await prismaClient.sum.create({
-        data: {
-            a: parsedResponse.data.a,
-            b: parsedResponse.data.b,
-            result: answer
-        }
-    })
+  const answer = parsedResponse.data.a + parsedResponse.data.b;
 
-    res.json({
-        answer
-    })
+  const result = await prismaClient.sum.create({
+    data: {
+      a: parsedResponse.data.a,
+      b: parsedResponse.data.b,
+      result: answer,
+    },
+  });
+
+  res.json({
+    answer,
+    id: result.id,
+  });
 });
 
 app.get("/sum", (req, res) => {
-    const parsedResponse = sumInput.safeParse({
-        a: Number(req.headers["a"]),
-        b: Number(req.headers["b"])
-    })
-    
-    if (!parsedResponse.success) {
-        return res.status(411).json({
-            message: "Incorrect inputs"
-        })
-    }
+  const parsedResponse = sumInput.safeParse({
+    a: Number(req.headers["a"]),
+    b: Number(req.headers["b"]),
+  });
 
-    const answer = parsedResponse.data.a + parsedResponse.data.b;
+  if (!parsedResponse.success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
 
-    res.json({
-        answer
-    })
+  const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+  res.json({
+    answer,
+  });
 });
--- Testing/Vitest/src/tests/index.test.ts
+++ Testing/Vitest/src/tests/index.test.ts
@@ -1,15 +1,34 @@
 import { describe, expect, test, it, vi } from "vitest";
 import request from "supertest";
 import { app } from "../index";
+import { prismaClient } from "../__mocks__/db";
 
 vi.mock("../db");
 
 describe("POST /sum", () => {
+  prismaClient.sum.create.mockResolvedValue({
+    id: 1,
+    a: 1,
+    b: 2,
+    result: 3,
+  });
+
+  vi.spyOn(prismaClient.sum, "create");
+
   it("should return the sum of two numbers", async () => {
     const res = await request(app).post("/sum").send({
       a: 1,
       b: 2,
     });
+
+    expect(prismaClient.sum.create).toHaveBeenCalledWith({
+      data: {
+        a: 1,
+        b: 2,
+        result: 3,
+      },
+    });
+
     expect(res.statusCode).toBe(200);
     expect(res.body.answer).toBe(3);
   });
